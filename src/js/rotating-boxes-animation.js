a3be82a8bcf90b8ca2141412b50616a7e9d9790d
GaJin Kim <gajin2000@gmail.com>
2022-10-10 21:40:40 -0500

    cleanup

--- src/js/rotating-boxes-animation.js
+++ src/js/rotating-boxes-animation.js
@@ -1,19 +1,21 @@
 generateRotationBoxes();
 
-$(window).resize(function() {generateRotationBoxes()});
+$(window).resize(function () {
+  generateRotationBoxes();
+});
 
 function generateRotationBoxes() {
-  console.log('hi'); 
+  console.log("hi");
   function repeat() {
     $("#repeating").append($(".rotating.box:first").clone());
   }
 
-  let fill = Math.floor(
-    (window.innerHeight * window.innerWidth) /
-      ($(".rotating.box:first").width() * $(".rotating.box:first").height())
-  );
+  let boxesPerRow = Math.floor(window.innerWidth / 115);
+  let boxesPerColumn = Math.floor(window.innerHeight / 115);
+  let fill = boxesPerRow * boxesPerColumn - 1;
 
   for (let i = 1; i < fill + 1; i++) {
     repeat();
   }
 }
+
