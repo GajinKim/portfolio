8d8769922d1b42fac41b4fff7d774a484904d8d1
GaJin Kim <gajin2000@gmail.com>
2022-10-11 17:30:45 -0500

    pop off king

--- src/js/rotating-boxes-animation.js
+++ src/js/rotating-boxes-animation.js
@@ -4,18 +4,17 @@ $(window).resize(function () {
   generateRotationBoxes();
 });
 
-function generateRotationBoxes() {
-  console.log("hi");
-  function repeat() {
-    $("#repeating").append($(".rotating.box:first").clone());
-  }
+function repeatBoxes() {
+  $("#repeating").append($(".rotating.box:first").clone());
+}
 
+function generateRotationBoxes() {
   let boxesPerRow = Math.floor(window.innerWidth / 115);
   let boxesPerColumn = Math.floor(window.innerHeight / 115);
   let fill = boxesPerRow * boxesPerColumn - 1;
 
   for (let i = 1; i < fill + 1; i++) {
-    repeat();
+    repeatBoxes();
   }
 }
 
